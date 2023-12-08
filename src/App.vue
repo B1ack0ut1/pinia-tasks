7c18b8d45684fab685c5a27034db925a6e1370d0
B1ack0ut1 <42845068+B1ack0ut1@users.noreply.github.com>
2023-12-08 18:17:24 +0800

    Accessed and used store state, added some global css

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,25 @@
       <img src="./assets/pinia-logo.svg" alt="pinia logo" />
       <h1>Pinia Tasks</h1>
     </header>
+
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
-export default {};
+import { useTaskStore } from "./stores/TaskStore";
+import TaskDetails from "./components/TaskDetails.vue";
+
+export default {
+  components: { TaskDetails },
+  setup() {
+    const taskStore = useTaskStore();
+    return { taskStore };
+  },
+};
 </script>
