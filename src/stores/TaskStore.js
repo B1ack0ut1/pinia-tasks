ccdf98cc7e8d6654a6f18b57830a98a1e7153c22
B1ack0ut1 <42845068+B1ack0ut1@users.noreply.github.com>
2023-12-09 18:30:58 +0800

    Added some data, fetched action tmade actions in store async for how it'd be with a db

--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -3,9 +3,10 @@ import { defineStore } from "pinia";
 export const useTaskStore = defineStore("taskStore", {
   state: () => ({
     tasks: [
-      { id: 1, title: "buy some milk", isFav: false },
-      { id: 2, title: "play Gloomhaven", isFav: true },
+      // { id: 1, title: "buy some milk", isFav: false },
+      // { id: 2, title: "play Gloomhaven", isFav: true },
     ],
+    loading: false,
   }),
   getters: {
     favs() {
@@ -21,15 +22,47 @@ export const useTaskStore = defineStore("taskStore", {
     },
   },
   actions: {
-    addTask(task) {
+    async getTasks() {
+      this.loading = true;
+      const res = await fetch("http://localhost:3000/tasks");
+      const data = await res.json();
+
+      this.tasks = data;
+      this.loading = false;
+    },
+    async addTask(task) {
       this.tasks.push(task);
+
+      const res = await fetch("http://localhost:3000/tasks", {
+        method: "POST",
+        body: JSON.stringify(task),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (res.error) {
+        console.log(res.error);
+      }
     },
-    deleteTask(id) {
+    async deleteTask(id) {
       this.tasks = this.tasks.filter((task) => task.id !== id);
+
+      const res = await fetch(`http://localhost:3000/tasks/${id}`, {
+        method: "DELETE",
+      });
     },
-    toggleFav(id) {
+    async toggleFav(id) {
       const task = this.tasks.find((task) => task.id === id);
       task.isFav = !task.isFav;
+
+      const res = await fetch(`http://localhost:3000/tasks/${id}`, {
+        method: "PATCH",
+        body: JSON.stringify({ isFav: task.isFav }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (res.error) {
+        console.log(res.error);
+      }
     },
   },
 });
